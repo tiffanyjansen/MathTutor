e19c6338fafd0481710762fcfc6ddd15a6f8213b
tiffany_jansen <tjansen15@mail.wou.edu>
2019-02-15 13:06:36 -0800

    Working on stuff

--- MathCenter/MathCenter/Controllers/HomeController.cs
+++ MathCenter/MathCenter/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using MathCenter.Models;
+using MathCenter.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -13,77 +14,88 @@ namespace MathCenter.Controllers
         //Access to Database.
         MathContext db = new MathContext();
 
-
         /*
          * The "Home Page." The page for Tutors/Faculty to either access the 
-         * sign in sheet or the data. (Depending on which you are.)
+         * sign in sheet or the data. (Depending on which you are.) It will 
+         * check passwords and return the view necessary for who signed in.
          */
         public ActionResult Index()
         {
             return View();
         }
-
         [HttpPost]
-        public ActionResult Index(string button, string tutorPwd, string facultyPwd)
+        public ActionResult Index(string button, string tutorPwd, string facultyPwd, int? Week)
         {
             //Get the passwords from the outside file so the passwords are not hard-coded.
+            //Hard code passwords since the outside file was being dumb.
             string tutorPass = "Math42";
                 //System.Configuration.ConfigurationManager.AppSettings["TutorPass"];
             string facultyPass = "Math42";
                 //System.Configuration.ConfigurationManager.AppSettings["FacultyPass"];
 
-            Debug.WriteLine(button);
+            //Check which button was pressed.
             if (button == "tutor")
             {
-                Debug.WriteLine(tutorPwd);
-                Debug.WriteLine(tutorPass);
-                if (tutorPwd == tutorPass)
+                //Check the password and make sure there is a week input.
+                if (tutorPwd == tutorPass && Week != -1)
                 {
-                    return RedirectToAction("SignIn");
+                    return RedirectToAction("SignIn", new { Week = Week});
                 }
-                else
+                //Return specific errors if the input is not valid.
+                else if(tutorPwd != tutorPass)
                 {
                     ViewBag.Error = "You typed in the wrong password. Please Try Again.";
                     return View();
                 }
+                else
+                {
+                    ViewBag.Error = "You did not select a week, please select a week number.";
+                    return View();
+                }
             }
-            if(button == "faculty")
+            else
             {
-                Debug.WriteLine(facultyPwd);
-                Debug.WriteLine(facultyPass);
+                //Check the password.
                 if (facultyPwd == facultyPass)
                 {
                     return RedirectToAction("FacultyStuff");
                 }
+                //Return specific errors if the input is not valid.
                 else
                 {
                     ViewBag.Error = "Please Try Again. You typed in the wrong password.";
                     return View();
                 }
             }
-            return View();
         }
 
         /*
-         * The method for signing a student in. This page will just have the student
-         * enter their V Number, then it will direct them to select their class and 
-         * add them to the database.
-         */
+        * The method for signing a student in. This page will just have the student
+        * enter their V Number, then it will direct them to select their class and 
+        * add them to the database.
+        */
         [HttpGet]
-        public ActionResult SignIn(string VNum)
+        public ActionResult SignIn(int Week)
         {
+            ViewBag.Num = Week;
+            return View();
+        }             
+        [HttpPost]
+        public ActionResult SignIn(string VNum, int Week)
+        {                
             //If no input, refresh page.
-            if (VNum == null)
+            if (VNum == null || VNum.Length != 8)
             {
+                ViewBag.Error = "Your V Number is invalid. It must be 8 characters. Please do not include the V.";
                 return View();
-            }                  
+            }                
             //If student is in the database, redirect to done page.
             if (db.Students.Find(VNum) != null)
             {
-                return RedirectToAction("Done");
+                return RedirectToAction("Done", new WeekVNum { Week = Week, VNum = VNum});
             }
             //Otherwise, redirect to create student page.
-            return RedirectToAction("CreateStudent", new { Id = VNum });
+            return RedirectToAction("NameInput", new WeekVNum { Week = Week, VNum = VNum});
         }
     }
 }
---
+++ MathCenter/MathCenter/Models/ViewModels/WeekVNum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MathCenter.Models.ViewModels
+{
+    public class WeekVNum
+    {
+        
+        public int Week { get; set; }
+
+        
+        public string VNum { get; set; }
+    }
+}
