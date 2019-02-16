5b577aca9f37cd0bd66ccfecc8d6037433896424
tiffany_jansen <tjansen15@mail.wou.edu>
2019-02-16 12:46:21 -0800

    I will continure this later

--- MathCenter/MathCenter/Scripts/database_java.js
+++ MathCenter/MathCenter/Scripts/database_java.js
@@ -1,4 +1,4 @@
-﻿$('#VButton').onclick(function (e) {
+﻿function SignIn() {
     var VNumber = $('#VNum').val.ToString();
     if (VNumber.length != 9) {
         error();
@@ -6,7 +6,7 @@
     else {
         success();
     }
-})
+}
 
 function error() {
     $('#Error h4').add("Your V Number is invalid. It must be 8 characters. Please do not include the V.");
