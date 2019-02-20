19442df06f67e5354a03fd5bd97a33d26501f65c
tiffany_jansen <tjansen15@mail.wou.edu>
2019-02-20 11:00:46 -0800

    working on the 'reload' part I want to implement

---
+++ MathCenter/MathCenter/Scripts/reloadPage.js
@@ -0,0 +1,15 @@
+﻿var reload_call = function(){
+    var pageURL = window.location.href;
+    console.log(pageURL);
+    var URLList = pageURL.split('/');
+    var id = URLList[URLList.length - 1];
+    console.log(id);
+    var source = "/Home/SignIn/" + id;
+    console.log(source);
+
+}
+
+var interval = 10000;
+console.log("interval = " + interval);
+window.setInterval(reload_call, interval);
+console.log("We are in the Javascript!")
