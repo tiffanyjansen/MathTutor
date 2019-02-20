16b5616880cb93c73ce9570900ce86e55b1855db
tiffany_jansen <tjansen15@mail.wou.edu>
2019-02-20 11:16:53 -0800

    Got the redirect part DONE!

--- MathCenter/MathCenter/Scripts/reloadPage.js
+++ MathCenter/MathCenter/Scripts/reloadPage.js
@@ -1,15 +1,12 @@
-﻿var reload_call = function(){
-    var pageURL = window.location.href;
-    console.log(pageURL);
-    var URLList = pageURL.split('/');
-    var id = URLList[URLList.length - 1];
-    console.log(id);
-    var source = "/Home/SignIn/" + id;
-    console.log(source);
-
+﻿//The function for redirecting the page to the "Sign In" Page.
+var reload_call = function () {
+    var pageURL = window.location.href; //Get the current url.
+    var URLList = pageURL.split('/'); //Split the url Up by /s
+    var id = URLList[URLList.length - 1];//Get the last part of the url (Week #)
+    var source = "/Home/SignIn/" + id; //Create the new source to be redirected to
+    window.location.href = source;//Use it to redirect there.
 }
 
-var interval = 10000;
-console.log("interval = " + interval);
-window.setInterval(reload_call, interval);
-console.log("We are in the Javascript!")
+//This is the timing part, so the page is only there for 'x' seconds before redirecting.
+var interval = 5000; //This is the 'x' part. It will be (interval/1000) seconds.
+window.setInterval(reload_call, interval); //Call the function above on the given interval.
