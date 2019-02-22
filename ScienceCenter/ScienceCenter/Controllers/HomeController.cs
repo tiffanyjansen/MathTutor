a3d1612c20a8a8c2a4563bbef76dcfab966ddd08
tiffany_jansen <tjansen15@mail.wou.edu>
2019-02-21 16:24:56 -0800

    copied over the home page

--- ScienceCenter/ScienceCenter/Controllers/HomeController.cs
+++ ScienceCenter/ScienceCenter/Controllers/HomeController.cs
@@ -8,23 +8,57 @@ namespace ScienceCenter.Controllers
 {
     public class HomeController : Controller
     {
+        /*
+        * The "Home Page." The page for Tutors/Faculty to either access the 
+        * sign in sheet or the data. (Depending on which you are.) It will 
+        * check passwords and return the view necessary for who signed in.
+        */
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Index(string button, string tutorPwd, string facultyPwd, int? Week)
         {
-            ViewBag.Message = "Your application description page.";
+            //Get the passwords
+            string tutorPass = "Science42"; 
+            string facultyPass = "Science42";
 
-            return View();
+            //Check which button was pressed.
+            if (button == "tutor")
+            {
+                //Check the password and make sure there is a week input.
+                if (tutorPwd == tutorPass && Week != -1)
+                {
+                    return RedirectToAction("SignIn", new { Week });
+                }
+                //Return specific errors if the input is not valid.
+                else if (tutorPwd != tutorPass)
+                {
+                    ViewBag.Error = "You typed in the wrong password. Please Try Again.";
+                    return View();
+                }
+                else
+                {
+                    ViewBag.Error = "You did not select a week, please select a week number.";
+                    return View();
+                }
+            }
+            else
+            {
+                //Check the password.
+                if (facultyPwd == facultyPass)
+                {
+                    return RedirectToAction("Index", "Faculty");
+                }
+                //Return specific errors if the input is not valid.
+                else
+                {
+                    ViewBag.Error = "Please Try Again. You typed in the wrong password.";
+                    return View();
+                }
+            }
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
